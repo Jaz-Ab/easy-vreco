e58ad07280d623158bf61d455cdab0f75fb2de23
YaniraAB <arenazas2.2@hotmail.com>
2018-02-07 21:27:54 -0500

    añadiendo Api de googleMaps

--- js/app.js
+++ js/app.js
@@ -1,18 +1,22 @@
-let begin = () => {
+function initMap() {
   let $btnFindMe = $('#find-me');
   let $map = $('#map');
   let findMe = () => {
     //  encontrando latitud y longitud
     let location = (position) => {
-      let latitude =  position.coords.latitude;
+      let latitude = position.coords.latitude;
       let longitude = position.coords.longitude;
-      $map.html(`
-        <p>Latitud : ${latitude} <br>
-        Longitud : ${longitude}</p>
-      `)
+      let latLong = {
+        lat: latitude,
+        lng: longitude
+      };
+      let gMap = new google.maps.Map($map, {
+        zoom: 18,
+        center: latLong
+      });
     };
     let error = () => {
-      $map.html('<p> No se pudo obtener tu ubicación </p>')
+      $map.html('<p> No se pudo obtener tu ubicación </p>');
     };
     //  verificando si soporta geolocation
     if (navigator.geolocation) {
@@ -21,9 +25,6 @@ let begin = () => {
       $map.html('<p>No Soporta geolocation</p>');
     }
     navigator.geolocation.getCurrentPosition(location, error);
-
   };
-
   $btnFindMe.on('click', findMe);
 }
-$(document).ready(begin);
